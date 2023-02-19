6a48fbcbbefcb54a8bc7991caf620ba1911b68bf
PBolsz <111749731+PBolsz@users.noreply.github.com>
2023-02-19 17:25:34 +0700

    Update RZDCARGTA.data.js

--- RZDCARGTA.data.js
+++ RZDCARGTA.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'T:/UE projects/RZDCARGTA/Binaries/HTML5/RZDCARGTA.data';
-    var REMOTE_PACKAGE_BASE = 'RZDCARGTA.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/r2f8hj4fp0ozaep/RZDCARGTA.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/r2f8hj4fp0ozaep/RZDCARGTA.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
